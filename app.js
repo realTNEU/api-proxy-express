760bda7e082cf1ed81e5c93b9452c9b3de46e03b
Ameya Taneja <ameyataneja1302@gmail.com>
2024-10-03 02:21:10 +0530

    updated to work for multiple apis

--- app.js
+++ app.js
@@ -3,47 +3,39 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const morgan = require('morgan');
 const cors = require('cors');
 
-const app1 = express();
-const app2 = express();
+const app = express();
 
-app1.use(cors());
-app2.use(cors());
+app.use(cors());
+app.use(morgan('dev'));
 
-app1.use(morgan('dev'));
-app2.use(morgan('dev'));
-
-const apiRoutes1 = {
-    '/api/ip2whois': {
-        target: 'https://api.ip2whois.com/v2', 
-        changeOrigin: true,
-        pathRewrite: { '^/api/ip2whois': '' }, 
-    },
+const apiRoutes = {
+  '/api/ip2whois': {
+    target: 'https://api.ip2whois.com/v2',
+    changeOrigin: true,
+    pathRewrite: { '^/api/ip2whois': '' },
+  },
+  '/api/shodan': {
+    target: 'https://api.shodan.io/shodan/host',
+    changeOrigin: true,
+    pathRewrite: { '^/api/shodan': '' },
+  },
+  '/api/ssl': {
+    target: 'https://ssl-certificate-checker2.p.rapidapi.com/ssl-certificate-checker/check',
+    changeOrigin: true,
+    pathRewrite: { '^/api/ssl': '' },
+    headers: {
+        "x-rapidapi-key": "facc61e5b8msh4021d32a5208244p1ecde7jsnc6676ad1119f",
+        "x-rapidapi-host": "ssl-certificate-checker2.p.rapidapi.com",
+    }
+  },
 };
 
-const apiRoutes2 = {
-    '/api/shodan': {
-        target: 'https://api.shodan.io/shodan/host',
-        changeOrigin: true,
-        pathRewrite: { '^/api/shodan': '' }, 
-    },
-};
-
-Object.keys(apiRoutes1).forEach((route) => {
-    app1.use(route, createProxyMiddleware(apiRoutes1[route]));
+Object.keys(apiRoutes).forEach((route) => {
+  app.use(route, createProxyMiddleware(apiRoutes[route]));
 });
 
-Object.keys(apiRoutes2).forEach((route) => {
-    app2.use(route, createProxyMiddleware(apiRoutes2[route]));
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Proxy server is running on http://localhost:${PORT}`);
 });
-
-const PORT1 = 5000; 
-const PORT2 = 5001; 
-
-app1.listen(PORT1, () => {
-    console.log(`Proxy server for Service 1 is running on http://localhost:${PORT1}`);
-});
-
-app2.listen(PORT2, () => {
-    console.log(`Proxy server for Service 2 is running on http://localhost:${PORT2}`);
-});
-
